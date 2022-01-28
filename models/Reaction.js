3f3d620db638c2ce8a54f22c444fe77e0cbdcbd1
TWAllison <tylerallison55@gmail.com>
2022-01-28 13:59:13 -0700

    death by typos

--- models/Reaction.js
+++ models/Reaction.js
@@ -2,7 +2,7 @@ const { Schema, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
 
-const reactionSchema = new schema(
+const reactionSchema = new Schema(
     {
         reactionId: {
             type: Schema.Types.ObjectId,
--- server.js
+++ server.js
@@ -17,6 +17,6 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/socialnetwork',
 
 mongoose.set('debug', true);
 
-application.use(require('./routes'));
+app.use(require('./routes'));
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
