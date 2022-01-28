b9bb2e157827b1304060404f935301098a0e549b
TWAllison <tylerallison55@gmail.com>
2022-01-28 13:27:09 -0700

    fixed missing require statement in server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const { application } = require('express');
 const express = require('express');
 const mongoose = require('mongoose');
-const routes = ('./routes');
+const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
