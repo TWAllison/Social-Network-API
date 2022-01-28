013a134551ea2a6274f759803576ad09dfdcccc9
TWAllison <tylerallison55@gmail.com>
2022-01-28 11:10:47 -0700

    psudo thoughts controllers

---
+++ controllers/though-controllers.js
@@ -0,0 +1,20 @@
+const { Thought, User } = require('../models');
+
+const thoughtControllers = {
+    // get  all thoughts
+
+    // get a single thought
+
+    // add thought
+
+    // update thought
+
+    // delete thought
+
+    // add reaction
+
+    // remove reaction
+
+};
+
+module.exports = thoughtControllers;
