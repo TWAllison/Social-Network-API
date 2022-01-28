0159c7ff3f963c3d8957455941fb7b6405c7915f
TWAllison <tylerallison55@gmail.com>
2022-01-28 09:45:25 -0700

    thought model

---
+++ models/Thought.js
@@ -0,0 +1,38 @@
+const { Schema, model, trusted } = require('mongoose');
+
+
+
+const thoughtScheam = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: 'You must leave a Thought!'
+            minlength: 1,
+            maxlength: 300
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: timestamp => dateFormat(timestamp)
+        },
+        username: {
+            type: String,
+            required: trusted,
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            getters: true
+        },
+        id: false
+    }
+);
+
+thoughtScheam.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
