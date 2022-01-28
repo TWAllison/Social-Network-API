5778456ac24a2a6820d2afe2999d92a0e8d13722
TWAllison <tylerallison55@gmail.com>
2022-01-28 09:31:22 -0700

    user model

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { schema, model, Schema } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, 'Must match an email address!'],
+        },
+        thoughts: [
+            {
+                type: schema.Types.ObjectId,
+                ref: 'Thoughts'
+            },
+        ],
+        friends: [
+            {
+                type: schema.Types.ObjectId,
+                ref: 'User' 
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtual: true,
+        },
+        id: false,
+    }
+);
+
+userScheam.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
