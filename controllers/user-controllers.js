5aa83fdb64ddefbaa433b202513bb3da17a5a327
TWAllison <tylerallison55@gmail.com>
2022-01-28 10:43:46 -0700

    user controller psodu

---
+++ controllers/user-controllers.js
@@ -0,0 +1,19 @@
+const { User, Thought } = require('../models');
+
+const userController = {
+    // get all users
+
+    // get single user
+
+    // new user
+
+    //update user
+
+    // delete user 
+
+    // add a friend 
+
+    // remove a friend
+};
+
+module.exports = userController;
